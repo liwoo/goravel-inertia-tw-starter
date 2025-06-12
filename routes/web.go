018ca3b9ce82de3a68b48e3b6e1864a82c16a907
Jeremiah Chienda <jeremiahchienda@gmail.com>
2025-06-13 00:24:52 +0200

    added redirect if authenticated

--- routes/web.go
+++ routes/web.go
@@ -19,18 +19,24 @@ func Web() {
 	dashboardController := controllers.NewDashboardController()
 
 	facades.Route().Post("/login", authController.Login)
+	facades.Route().Get("/login", func(ctx http.Context) http.Response {
+		return inertiaHelper.Render(ctx, "auth/Login", map[string]interface{}{
+			"version": support.Version,
+		})
+	})
 	//register una
 	facades.Route().Get("/una", utilController.ShowUnaPage)
+
+	// Public route for home/login, redirect to dashboard if already authenticated
+	facades.Route().Middleware(middleware.RedirectIfAuthenticated()).Get("/", func(ctx http.Context) http.Response {
+		return inertiaHelper.Render(ctx, "auth/Login", map[string]interface{}{
+			"version": support.Version,
+		})
+	})
 
 	// Authenticated routes
 	facades.Route().Middleware(middleware.JwtAuth()).Group(func(router route.Router) {
 		router.Post("/logout", authController.Logout)
-
-		router.Get("/", func(ctx http.Context) http.Response {
-			return inertiaHelper.Render(ctx, "auth/Login", map[string]interface{}{
-				"version": support.Version,
-			})
-		})
 
 		router.Get("/settings", func(ctx http.Context) http.Response {
 			return inertiaHelper.Render(ctx, "settings/Index", map[string]interface{}{
